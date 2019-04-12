d156cc728e62b8c2af4922b089cbbd2f7d184985
Konstantin BIFERT <konstantin.bifert@gmail.com>
2019-04-12 23:54:14 +0200

    Add ugly old css

--- pages/index.vue
+++ pages/index.vue
@@ -208,3 +208,31 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+*
+  box-sizing: content-box
+  // overflow: auto;
+body
+  margin: 0
+#app
+  font-family: "Avenir", Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+.main-content
+  width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+.wheel-block
+  position: relative
+  flex: 1 1 0
+.right-aside-block
+  display: flex
+  flex-direction: column
+  flex: 1 0 0
+  transform: translate3d(-25%, 0, 0)
+  border: 3px solid yellow
+</style>
